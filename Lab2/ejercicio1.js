0c061eabeeb28e160dffef188a77396833e87e41
Fer Vargas <fervargas@MacBook-Pro-de-Fer.local>
2020-08-20 18:42:52 -0500

    Finalización de primeros dos ejercicios

---
+++ Lab2/ejercicio1.js
@@ -0,0 +1,7 @@
+function ejercicio1 (let numero) {
+    document.write("<table>");
+    for(let i = 1, i < numero; i++){
+        document.write("<tr><td>"+i+"</td>"+"<td>"+i*i+"</td></tr>");
+        
+    }
+}
