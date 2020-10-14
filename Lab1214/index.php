50e73a3e576fc6e5c890f87f8d3678e767bbd230
FVargas4 <fervargasalv17@hotmail.com>
2020-10-14 17:45:43 -0500

    Concordancia

--- Lab1214/index.php
+++ Lab1214/index.php
@@ -3,8 +3,8 @@
 include("_header.html");
 include("_navbar.html");
 include("_registro_paciente.html");
-/*include("_multiregistro_paciente.html");*/
-include("_form_paciente.html");
+include("_multiregistro_paciente.html");
+include("_form_upload.html");
 include("_footer.html");
 
 ?>
