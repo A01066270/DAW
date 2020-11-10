ff0b7156572f5ee68f841a055441d022d1c39240
A01066270 <a01066270@itesm.mx>
2020-11-10 06:25:19 -0600

    Styling table

--- Consultorio/php/form.php
+++ Consultorio/php/form.php
@@ -42,168 +42,177 @@
     <div >
     <h5>El paciente...</h5>
    
-    <table class="select_questions">
-        <td>
-            <tr><label>Problemas de Presión</label></tr>
-            <tr><input type="radio" name="ppresion" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="ppresion" value="0"> 
-    <label for="0" style="color:gray" >No</label><br></tr>
+    <table class='select_questions' >
+        <tr>
+            <td><p>Tiene problemas de Presión?</p></td>
+            <td>
+            <input type="radio" name="ppresion" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="ppresion" value="0"> 
+                <label for="0" style="color:gray" >No</label><br></dr>
+        </tr>
+        <tr>
+            <td><label>Fuma</label></td>
+            <td><input type="radio" name="fuma" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="fuma" value="0"> 
+                <label for="0" style="color:gray" >No</label><br></td>
+        </tr>
+        <tr>
+            <td><label>Toma alcohol</label></td>
+            <td><input type="radio" name="alcohol" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="alcohol" value="0"> 
+                <label for="0" style="color:gray" >No</label><br></td>
+        </tr>
+        <tr>
+            <td><label>Consume drogas</label></td>
+            <td><input type="radio" name="drogas" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="drogas" value="0"> 
+                <label for="0" style="color:gray" >No</label><br></td>
+        </tr>
+        <tr>
+            <td><label>Tatuajes</label></td>
+            <td><input type="radio" name="tatuaje" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="tatuaje" value="0"> 
+                <label for="0" style="color:gray" >No</label><br></td>
+        </tr>
+        <tr>
+            <td><label>Problemas Renales</label></td>
+            <td><input type="radio" name="proren" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="proren" value="0"> 
+                <label for="0" style="color:gray" >No</label><br></td>
+        </tr>
+        <tr>
+            <td><label>Diabetes</label></td>
+            <td><input type="radio" name="diabetes" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="diabetes" value="0"> 
+                <label for="0" style="color:gray" >No</label><br></td>
+        </tr>
+        <tr>
+            <td><label>Hepatitis</label></td>
+            <td><input type="radio" name="hepatitis" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="hepatitis" value="0"> 
+                <label for="0" style="color:gray" >No</label><br></td>
         </td>
-        <td>
-            <tr><label>Fuma</label></tr>
-            <tr><input type="radio" name="fuma" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="fuma" value="0"> 
-    <label for="0" style="color:gray" >No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Toma alcohol</label></tr>
-            <tr><input type="radio" name="alcohol" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="alcohol" value="0"> 
-    <label for="0" style="color:gray" >No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Consume drogas</label></tr>
-            <tr><input type="radio" name="drogas" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="drogas" value="0"> 
-    <label for="0" style="color:gray" >No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Tatuajes</label></tr>
-            <tr><input type="radio" name="tatuaje" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="tatuaje" value="0"> 
-    <label for="0" style="color:gray" >No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Problemas Renales</label></tr>
-            <tr><input type="radio" name="proren" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="proren" value="0"> 
-    <label for="0" style="color:gray" >No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Diabetes</label></tr>
-            <tr><input type="radio" name="diabetes" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="diabetes" value="0"> 
-    <label for="0" style="color:gray" >No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Hepatitis</label></tr>
-            <tr><input type="radio" name="hepatitis" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="hepatitis" value="0"> 
-    <label for="0" style="color:gray" >No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Artritis</label></tr>
-            <tr><input type="radio" name="artritis" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="artritis" value="0"> 
-    <label for="0" style="color:gray" >No</label><br></tr>
-        </td>
+        <tr>
+            <td><label>Artritis</label></td>
+            <td><input type="radio" name="artritis" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="artritis" value="0"> 
+                <label for="0" style="color:gray" >No</label><br></td>
+        </tr>
 
-        <td>
-            <tr><label>Enfermedad Sanguinea</label></tr>
-            <tr><input type="radio" name="enfesang" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="enfesang" value="0"> 
-    <label for="0" style="color:gray">No</label><br></tr>
-        </td>
+        <tr>
+            <td><label>Enfermedad Sanguinea</label></td>
+            <td><input type="radio" name="enfesang" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="enfesang" value="0"> 
+                <label for="0" style="color:gray">No</label><br></td>
+        </tr>
 
-        <td>
-            <tr><label>Enfermedad Sistema Nervioso</label></tr>
-            <tr><input type="radio" name="enfesist" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="enfesist" value="0"> 
-    <label for="0" style="color:gray">No</label><br></tr>
-        </td>
-        <label>Enfermedad Respiratoria</label>
-    <input type="radio" name="enferesp" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="enferesp" value="0"> 
-    <label for="0" style="color:gray">No</label>
+        <tr>
+            <td><label>Enfermedad Sistema Nervioso</label></td>
+            <td><input type="radio" name="enfesist" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="enfesist" value="0"> 
+                <label for="0" style="color:gray">No</label><br></td>
+        </tr>
+        <tr>
+            <td>
+                <label>Enfermedad Respiratoria</label>
+            </td>
+            <td>
+                <input type="radio" name="enferesp" value="1"> 
+            <label for="1" style="color:gray">Si  </label>
+            <input type="radio" name="enferesp" value="0"> 
+            <label for="0" style="color:gray">No</label>
     
-    <label style="color:gray"> Cual?</label>
-    <input type="text" class="login-input" name="cualenferesp" placeholder="Asma, bronquitis, etc..."><br>
+        <label style="color:gray"> Cual?</label><br>
+            <input type="text" class="login-input" name="cualenferesp" placeholder="Asma, bronquitis, etc...">
+            </td>
+        </tr>
+        
+            <br>
+        <tr>
+            <td>
+                 <label>Alergia</label>
+            </td>
 
+            <td>
+                 <input type="radio" name="alergia" value="1"> 
+            <label for="1" style="color:gray">Si  </label>
+        <input type="radio" name="alergia" value="0"> 
+        <label for="0" style="color:gray">No</label>
+    
+    <label style="color:gray"> Cual?</label><br>    
+        <input type="text" class="login-input" name="cualalergia" placeholder="Cacahuate, gatos, etc..."><br>
+
+            </td>
+
+        </tr>
     <br>
 
-    <label>Alergia</label>
-    <input type="radio" name="alergia" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="alergia" value="0"> 
-    <label for="0" style="color:gray">No</label>
-    
-    <label style="color:gray"> Cual?</label>
-    <input type="text" class="login-input" name="cualalergia" placeholder="Cacahuate, gatos, etc..."><br>
+       
+           
 
+        <tr>
+            <td><label>Cancer</label></td>
+            <td><input type="radio" name="cancer" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="cancer" value="0"> 
+                <label for="0" style="color:gray">No</label><br></td>
+        </tr>
 
-        <td>
-            <tr><label>Cancer</label></tr>
-            <tr><input type="radio" name="cancer" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="cancer" value="0"> 
-    <label for="0" style="color:gray">No</label><br></tr>
-        </td>
-
-        <td>
-            <tr><label>Cirgía reciente</label></tr>
-            <tr><input type="radio" name="cirugmed" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="cirugmed" value="0"> 
-    <label for="0" style="color:gray">No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Tomado medicamento en los ultimos 2 meses</label></tr>
-            <tr><input type="radio" name="medicado" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="medicado" value="0"> 
-    <label for="0" style="color:gray">No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Tomado anticonceptivos en los ultimos 2 meses</label></tr>
-            <tr><input type="radio" name="anticoncept" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="anticoncepts" value="0"> 
-    <label for="0" style="color:gray">No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Estado Embarazada</label></tr>
-            <tr><input type="radio" name="embarazo" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="embarazo" value="0"> 
-    <label for="0" style="color:gray">No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Enfermedades de Transmisión Sexual</label></tr>
-            <tr><input type="radio" name="ETS" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="ETS" value="0"> 
-    <label for="0" style="color:gray">No</label><br></tr>
-        </td>
-        <td>
-            <tr><label>Ha viajado en las ultimas 2 semanas</label></tr>
-            <tr><input type="radio" name="viaje" value="1"> 
-    <label for="1" style="color:gray">Si  </label>
-    <input type="radio" name="viaje" value="0"> 
-    <label for="0" style="color:gray">No</label><br></tr>
-        </td>
-        <td>
-            <tr>
-            <input type="text" placeholder="Extras" class="login-input"><br></tr>
-        </td>
-
-
-
-
-
-
+        <tr>
+            <td><label>Cirgía reciente</label></td>
+            <td><input type="radio" name="cirugmed" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="cirugmed" value="0"> 
+                <label for="0" style="color:gray">No</label><br></td>
+        </tr>
+        <tr>
+            <td><label>Tomado medicamento en los ultimos 2 meses</label></td>
+            <td><input type="radio" name="medicado" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="medicado" value="0"> 
+                <label for="0" style="color:gray">No</label><br></td>
+        </tr>
+        <tr>
+            <td><label>Tomado anticonceptivos en los ultimos 2 meses</label></td>
+            <td><input type="radio" name="anticoncept" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="anticoncepts" value="0"> 
+                <label for="0" style="color:gray">No</label><br></td>
+        </tr>
+        <tr>
+            <td><label>Estado Embarazada</label></td>
+            <td><input type="radio" name="embarazo" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="embarazo" value="0"> 
+                <label for="0" style="color:gray">No</label><br></td>
+        </tr>
+        <tr>
+            <td><label>Enfermedades de Transmisión Sexual</label></td>
+            <td><input type="radio" name="ETS" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="ETS" value="0"> 
+                <label for="0" style="color:gray">No</label><br></td>
+        </tr>
+        <tr>
+            <td><label>Ha viajado en las ultimas 2 semanas</label></td>
+            <td><input type="radio" name="viaje" value="1"> 
+                <label for="1" style="color:gray">Si  </label>
+                <input type="radio" name="viaje" value="0"> 
+                <label for="0" style="color:gray">No</label><br></td>
+        </tr>
     </table>
+    <input type="text" placeholder="Extras" class="login-input" style='width:100%;'><br>
     <br>
 
 
